1fa1f9f36a2d6e8d7e22a41cf79cef180583509c
heyitsjay13swift-ai <heyitsjay13swift@gmail.com>
2026-01-19 00:12:55 +0530

    Added Modularity in existing code

--- transformations/notebook/01_bronze/yellow_taxi_trips_raw.py
+++ transformations/notebook/01_bronze/yellow_taxi_trips_raw.py
@@ -1,29 +1,32 @@
 # Databricks notebook source
+import sys
+import os
+# Go two levels up to reach the project root
+project_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))
+
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
 from pyspark.sql.functions import current_timestamp
 from dateutil.relativedelta import relativedelta
+from datetime import date
+from modules.utils.date_utils import get_target_yyyymm
+from modules.transformations.metadata import add_processed_timestamp
 
 # COMMAND ----------
-
-from datetime import date
 
 # Obtains the year-month for 2 months prior to the current month in yyyy-MM format
-two_months_ago = date.today() - relativedelta(months=2)
-formatted_date = two_months_ago.strftime("%Y-%m")
+formatted_date = get_target_yyyymm(2)
 
-# COMMAND ----------
-
-#Checking and loading data only 2 months prior (since source has lag of 2 months)
+# Read all Parquet files for the specified month from the landing directory into a DataFrame
 df = spark.read.format("parquet").load(f"/Volumes/nyc_taxi/00_landing/data_sources/nyctaxi_yellow/{formatted_date}")
 
 # COMMAND ----------
 
-#Adding field for timestamp information for data ingestion
-df = df.withColumn("processed_timestamp", current_timestamp())
+# Add a column to capture when the data was processed
+df = add_processed_timestamp(df)
 
 # COMMAND ----------
 
-#Appending new data to existing data in our managed delta table
-df.write.mode("append").saveAsTable("nyc_taxi.01_bronze.yellow_trips_raw")
-
-# COMMAND ----------
-
+# Write the DataFrame to a Unity Catalog managed Delta table in the bronze schema, appending the new data
+df.write.mode("append").saveAsTable("nyctaxi.01_bronze.yellow_trips_raw")
--- transformations/notebook/02_silver/yellow_trips_cleansed.py
+++ transformations/notebook/02_silver/yellow_trips_cleansed.py
@@ -1,18 +1,24 @@
 # Databricks notebook source
+import sys
+import os
+# Go two levels up to reach the project root
+project_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))
+
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
 from pyspark.sql.functions import col, when, timestamp_diff
 from datetime import date
 from dateutil.relativedelta import relativedelta
+from modules.utils.date_utils import get_month_start_n_months_ago
 
 # COMMAND ----------
 
-# Date filtering
-# Two months ago first day
-two_months_ago_start = date.today().replace(day=1) - relativedelta(months=2)
+# Get the first day of the month two months ago
+two_months_ago_start = get_month_start_n_months_ago(2)
 
 # Get the first day of the month one month ago
-one_month_ago_start = date.today().replace(day=1) - relativedelta(months=1)
-
-# COMMAND ----------
+one_month_ago_start = get_month_start_n_months_ago(1)
 
 # COMMAND ----------
 
@@ -25,53 +31,60 @@ df = spark.read.table("nyc_taxi.01_bronze.yellow_trips_raw").filter(f"tpep_picku
 
 # COMMAND ----------
 
-# COMMAND ----------
-
 # Select and transform fields, decoding codes and computing duration
-df = df.select (
+df = df.select(
+    # Map numeric VendorID to vendor names
     when(col("VendorID") == 1, "Creative Mobile Technologies, LLC")
-        .when(col("VendorID") == 2, "Curb Mobility, LLC")
-        .when(col("VendorID") == 6, "Myle Technologies Inc")
-        .when(col("VendorID") == 7, "Helix")
-        .otherwise("Unknown")
-        .alias("vendor"),
+      .when(col("VendorID") == 2, "Curb Mobility, LLC")
+      .when(col("VendorID") == 6, "Myle Technologies Inc")
+      .when(col("VendorID") == 7, "Helix")
+      .otherwise("Unknown")
+      .alias("vendor"),
+    
     "tpep_pickup_datetime",
     "tpep_dropoff_datetime",
+    # Calculate trip duration in minutes
+    timestamp_diff('MINUTE', df.tpep_pickup_datetime, df.tpep_dropoff_datetime).alias("trip_duration"),
     "passenger_count",
     "trip_distance",
+
+    # Decode rate codes into readable rate types
     when(col("RatecodeID") == 1, "Standard Rate")
-        .when(col("RatecodeID") == 2, "JFK")
-        .when(col("RatecodeID") == 3, "Newark")
-        .when(col("RatecodeID") == 4, "Nassau or Westchester")
-        .when(col("RatecodeID") == 5, "Negotiated fare")
-        .when(col("RatecodeID") == 6, "Group ride")
-        .otherwise("Unknown")
-        .alias("rate_type"),
+      .when(col("RatecodeID") == 2, "JFK")
+      .when(col("RatecodeID") == 3, "Newark")
+      .when(col("RatecodeID") == 4, "Nassau or Westchester")
+      .when(col("RatecodeID") == 5, "Negotiated Fare")
+      .when(col("RatecodeID") == 6, "Group Ride")
+      .otherwise("Unknown")
+      .alias("rate_type"),
+    
     "store_and_fwd_flag",
+    # alias columns for consistent naming convention
     col("PULocationID").alias("pu_location_id"),
     col("DOLocationID").alias("do_location_id"),
-    when(col("payment_type") == 0, "Flex fare trip")
-        .when(col("payment_type") == 1, "Credit card")
-        .when(col("payment_type") == 2, "Cash")
-        .when(col("payment_type") == 3, "No Charge")
-        .when(col("payment_type") == 4, "Dispute")
-        .when(col("payment_type") == 6, "Voided trip")
-        .otherwise("Unknown")
-        .alias("payment_type"),
+    
+    # Decode payment types
+    when(col("payment_type") == 0, "Flex Fare trip")
+      .when(col("payment_type") == 1, "Credit card")
+      .when(col("payment_type") == 2, "Cash")
+      .when(col("payment_type") == 3, "No charge")
+      .when(col("payment_type") == 4, "Dispute")
+      .when(col("payment_type") == 6, "Voided trip")
+      .otherwise("Unknown")
+      .alias("payment_type"),
+    
     "fare_amount",
     "extra",
     "mta_tax",
-    "tip_amount",
     "tolls_amount",
     "improvement_surcharge",
     "total_amount",
     "congestion_surcharge",
-    "Airport_fee",
+    # alias columns for consistent naming convention
+    col("Airport_fee").alias("airport_fee"),
     "cbd_congestion_fee",
     "processed_timestamp"
-    )
-
-# COMMAND ----------
+)
 
 # COMMAND ----------
 
